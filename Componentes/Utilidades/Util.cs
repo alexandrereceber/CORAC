5274655ee8c97d71220586dec1608a61652d7e45
alexandre marques <alexandrereceber@gmail.com>
2020-05-30 08:57:25 -0300

    tela capturewindow api windows

--- Componentes/Utilidades/Util.cs
+++ Componentes/Utilidades/Util.cs
@@ -239,6 +239,77 @@ namespace ServerClienteOnline.Utilidades
     }
     public class Pacote_FrameTelas : ITipoPacote
     {
+        private class GDI32
+        {
+
+            public const int SRCCOPY = 0x00CC0020; // BitBlt dwRop parameter
+            [DllImport("gdi32.dll")]
+            public static extern bool BitBlt(IntPtr hObject, int nXDest, int nYDest,
+                int nWidth, int nHeight, IntPtr hObjectSource,
+                int nXSrc, int nYSrc, int dwRop);
+            [DllImport("gdi32.dll")]
+            public static extern IntPtr CreateCompatibleBitmap(IntPtr hDC, int nWidth,
+                int nHeight);
+            [DllImport("gdi32.dll")]
+            public static extern IntPtr CreateCompatibleDC(IntPtr hDC);
+            [DllImport("gdi32.dll")]
+            public static extern bool DeleteDC(IntPtr hDC);
+            [DllImport("gdi32.dll")]
+            public static extern bool DeleteObject(IntPtr hObject);
+            [DllImport("gdi32.dll")]
+            public static extern IntPtr SelectObject(IntPtr hDC, IntPtr hObject);
+        }
+
+        private class User32
+        {
+
+            [StructLayout(LayoutKind.Sequential)]
+            public struct RECT
+            {
+                public int left;
+                public int top;
+                public int right;
+                public int bottom;
+            }
+            [DllImport("user32.dll")]
+            public static extern IntPtr GetDesktopWindow();
+            [DllImport("user32.dll")]
+            public static extern IntPtr GetWindowDC(IntPtr hWnd);
+            [DllImport("user32.dll")]
+            public static extern IntPtr ReleaseDC(IntPtr hWnd, IntPtr hDC);
+            [DllImport("user32.dll")]
+            public static extern IntPtr GetWindowRect(IntPtr hWnd, ref RECT rect);
+        }
+        public Image CaptureWindow(IntPtr handle)
+        {
+            // get te hDC of the target window
+            IntPtr hdcSrc = User32.GetWindowDC(handle);
+            // get the size
+            User32.RECT windowRect = new User32.RECT();
+            User32.GetWindowRect(handle, ref windowRect);
+            int width = windowRect.right - windowRect.left;
+            int height = windowRect.bottom - windowRect.top;
+            // create a device context we can copy to
+            IntPtr hdcDest = GDI32.CreateCompatibleDC(hdcSrc);
+            // create a bitmap we can copy it to,
+            // using GetDeviceCaps to get the width/height
+            IntPtr hBitmap = GDI32.CreateCompatibleBitmap(hdcSrc, width, height);
+            // select the bitmap object
+            IntPtr hOld = GDI32.SelectObject(hdcDest, hBitmap);
+            // bitblt over
+            GDI32.BitBlt(hdcDest, 0, 0, width, height, hdcSrc, 0, 0, GDI32.SRCCOPY);
+            // restore selection
+            GDI32.SelectObject(hdcDest, hOld);
+            // clean up 
+            GDI32.DeleteDC(hdcDest);
+            User32.ReleaseDC(handle, hdcSrc);
+            // get a .NET image object for it
+            Image img = Image.FromHbitmap(hBitmap);
+            // free up the Bitmap object
+            GDI32.DeleteObject(hBitmap);
+            return img;
+        }
+
         /**
          * Classe convertida em json e enviada para o cliente
          */
@@ -313,32 +384,12 @@ namespace ServerClienteOnline.Utilidades
 
         public void GerarTelas()
         {
-            var LogueUI = Process.GetProcessesByName("LogonUI").Length;
-            if(LogueUI == 0)
-            {
-                MemoryStream TransformImg = new MemoryStream();
+            Image Tela = CaptureWindow(User32.GetDesktopWindow());
 
-                foreach (Tela TL in FrameTelas)
-                {
-                    if (Configuracoes_Gerais.Primary == TL.Monitor)
-                    {
-                        TL.CopyTela.CopyFromScreen(TL.P, new Point { X = 0, Y = 0 }, TL.T);
-                        TransformImg = new MemoryStream();
-                        TL.TelaMonitor.Save(TransformImg, Configuracoes_Gerais.TiposImagems);
-                        TL.Primary = Convert.ToBase64String(TransformImg.ToArray());
-
-                    }
-                    else
-                    {
-                        TL.CopyTela.CopyFromScreen(TL.P, new Point { X = 0, Y = 0 }, TL.T);
-                        TransformImg = new MemoryStream();
-                        TL.TelaMonitor.Save(TransformImg, Configuracoes_Gerais.TiposImagems);
-                        TL.ThumbnailImage = Convert.ToBase64String(TransformImg.ToArray());
-
-                    }
-                }
-            }
-            
+            FrameTelas[0].CopyTela.CopyFromScreen(new Point { X = 0, Y = 0 }, new Point { X = 0, Y = 0 }, new Size { Width = 300, Height = 300 });
+            MemoryStream TransformImg = new MemoryStream();
+            FrameTelas[0].TelaMonitor.Save(TransformImg, Configuracoes_Gerais.TiposImagems);
+            FrameTelas[0].Primary = Convert.ToBase64String(TransformImg.ToArray());
         }
     }
 
